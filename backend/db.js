f593772069960875f3610c416246544445672a1f
mya574 <m.mokrani@ecole-ipssi.net>
2025-01-07 10:15:48 +0100

    feat(controller):ajou conex bdd et reservation soin

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion à la base de données :', err);
+        throw err;
+    }
+    console.log('Connecté à la base de données MySQL');
+});
+
+module.exports = db;
--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,21 @@
 const express = require('express');
-const mysql = require('mysql');
 const bodyParser = require('body-parser');
-
 require('dotenv').config();
 
 const app = express();
+app.use(bodyParser.json()); // middleware 
 
-app.use(bodyParser.json()); // Middleware pour analyser les requêtes JSON
+// routes des fichier 
+const userRoutes = require('./user'); 
+const soinRoutes = require('./reservations/soin'); 
 
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
-});
+// routes
+app.use('/users', userRoutes); 
+app.use('/soin', soinRoutes); 
 
-// Se connecter à la base de données
-db.connect((err) => {
-    if (err) {
-        console.error('Erreur de connexion à la base de données :', err);
-        return;
-    }
-    console.log('Connecté à la base de données MySQL');
-});
-
-// Endpoint pour ajouter des données dans une table
-app.post('/add', (req, res) => {
-    const { nom, prenom, email } = req.body; // Exemple de champs à insérer id-utilisateur	nom-utilisateur	prenom-utilisateur
-    const sql = 'INSERT INTO utilisateurs (nom_utilisateur, prenom_utilisateur, email, is_admin) VALUES (?, ?, ?, true)';
-    db.query(sql, [nom, prenom, email], (err, result) => {
-        if (err) {
-            console.error('Erreur lors de l\'ajout des données :', err);
-            res.status(500).send('Erreur serveur');
-            return;
-        }
-        res.send({ message: 'Données ajoutées avec succès', id: result.insertId });
-    });
+// par défaut
+app.get('/', (req, res) => {
+    res.send('Serveur fonctionnel');
 });
 
 const port = process.env.PORT || 3000;
